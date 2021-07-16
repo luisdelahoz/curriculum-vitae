8bee263f5d2a732db8af426ef8f026d86f2b7ac9
Luis De La Hoz <luisgdelahozl@gmail.com>
2021-07-16 18:11:29 -0300

    fix: Use grid system (row and columns) in description component

--- src/components/description/index.tsx
+++ src/components/description/index.tsx
@@ -3,9 +3,11 @@ import { FULL_NAME } from 'src/config';
 import styled from 'styled-components';
 import ProfilePicture from 'src/assets/images/profile-picture.jpg';
 import ContactInformation from './contact-information';
+import { Column } from '../grid';
 
 const Container = styled.div`
   display: flex;
+  flex-wrap: wrap;
   margin-left: 5rem;
   margin-right: 5rem;
   padding-top: 1rem;
@@ -13,14 +15,13 @@ const Container = styled.div`
 
 const Summary = styled.div`
   padding: 1em;
-  width: 50%;
 `;
 
 const ProfileImageContainer = styled.div`
   align-items: center;
   display: flex;
+  height: 100%;
   justify-content: center;
-  width: 50%;
 `;
 
 const ProfileImage = styled.img`
@@ -40,24 +41,28 @@ const BriefDescription = styled.p`
 function Description(): JSX.Element {
   return (
     <Container>
-      <Summary>
-        <Presentation>I&apos;m {FULL_NAME}</Presentation>
-        <BriefDescription>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
-        </BriefDescription>
-        <ContactInformation />
-      </Summary>
-      <ProfileImageContainer>
-        <ProfileImage src={ProfilePicture} alt="Profile" />
-      </ProfileImageContainer>
+      <Column mobile={12} medium={6}>
+        <Summary>
+          <Presentation>I&apos;m {FULL_NAME}</Presentation>
+          <BriefDescription>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry standard dummy text ever
+            since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </BriefDescription>
+          <ContactInformation />
+        </Summary>
+      </Column>
+      <Column mobile={12} medium={6}>
+        <ProfileImageContainer>
+          <ProfileImage src={ProfilePicture} alt="Profile" />
+        </ProfileImageContainer>
+      </Column>
     </Container>
   );
 }
